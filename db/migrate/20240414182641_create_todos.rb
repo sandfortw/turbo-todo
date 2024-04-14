f88a77fe51336a41971e1f55794cd01174e06aa5
Weston Sandfort <sandfortw@gmail.com>
2024-04-14 13:05:29 -0600

    Create db

--- db/migrate/20240414182641_create_todos.rb
+++ db/migrate/20240414182641_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[7.1]
   def change
     create_table :todos do |t|
       t.string :name
-      t.integer :status
+      t.integer :status, default: 0
 
       t.timestamps
     end
